eef5de82465f6301e977e1c9b0e9d72a8663839c
Tyler Corsair <tyler@tylercorsair.com>
2017-10-15 04:26:25 -0700

    Update Welcome.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>Hi there! There'll be more here soon, in the meantime please join our <a href='https://discord.gg/WhmT8dU'>Discord</a>!</div>
-    <div>안녕! 곧 더 많은 것이있을 것이며, 그 사이에 <a href='https://discord.gg/WhmT8dU'>우리의 불화에</a> 참여하십시오!</div>
+    <h4>Greetings.</h4>
+    <p>This is a work in progress, in the meanwhile join us on our <a href='https://discord.gg/WhmT8dU' class="btn btn-soft btn-sm"><i class="fab fa-discord"></i> Discord</a></p>
+    <p class="mb-0">안녕! 곧 더 많은 것이있을 것이며, 그 사이에 <a href='https://discord.gg/WhmT8dU' class="btn btn-soft btn-sm"><i class="fab fa-discord"></i> 우리의 불화에</a> 참여하십시오!</p>
 @endsection
