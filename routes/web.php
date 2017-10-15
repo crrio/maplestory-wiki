a5bb5f3e295f17546036051fbd14e53cb8a58af3
Tyler Corsair <tyler@tylercorsair.com>
2017-10-15 04:26:32 -0700

    Update Item List route.

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/{region}/{version}/item', function (Request $request, $region, $version) {
+Route::get('/{region}/{version}/items', function (Request $request, $region, $version) {
     $query = [];
     $oldData = [];
 
