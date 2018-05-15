485de67c26fdc46b6b38e48b0071a00b99cad2bb
Tyler Corsair <tyler@tylercorsair.com>
2018-05-14 18:24:35 -0700

    Update front page.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,11 @@
 @endsection
 
 @section('content')
-    <h4>Greetings.</h4>
-    <p>This is a work in progress, in the meanwhile join us on our <a href='https://discord.gg/WhmT8dU' class="btn btn-soft btn-sm"><i class="fab fa-discord"></i> Discord</a></p>
+    <img src="http://maplestory.io/api/gms/latest/mob/100006/icon?resize=5" class="float-right ml-4"/>
+    <h1 class="mt-2">
+        Welcome.
+    </h1>
+    <p class="lead">We are the the <b>only</b> complete, thorough, and always up-to-date knowledge base for the MMORPG Maplestory. Discover everything about the game all in one place.</p>
+    <p>This service is a work in progress, in the meanwhile join us on our <a href='https://discord.gg/WhmT8dU' class="btn btn-soft btn-sm"><i class="fab fa-discord"></i> Discord</a></p>
     <p class="mb-0">안녕! 곧 더 많은 것이있을 것이며, 그 사이에 <a href='https://discord.gg/WhmT8dU' class="btn btn-soft btn-sm"><i class="fab fa-discord"></i> 우리의 불화에</a> 참여하십시오!</p>
 @endsection
